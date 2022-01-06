bf79544c2c7040b03a83e062fa1a6e98004220ea
fatemah-alm <fatemah.alm@hotmail.com>
2022-01-07 00:55:54 +0400

    done task

--- index.js
+++ index.js
@@ -8,4 +8,43 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+let bmiCalc = (weight)/((height/100)^2);
+alert(bmiCalc);
+
+if(bmiCalc < 18.5){
+  alert("you are UNDERWEIGHT!!! you flat stick");
+}
+else if(18.5<bmiCalc && bmiCalc<24.9)
+{
+  alert("YOU ARE HEALTHY!!!! daaaaaaamn")
+}
+else if(25.0<bmiCalc && bmiCalc<29.9){
+  alert("YOU ARE OVERWEIGHT!!!! start a diet!")
+}
+else if (30<bmiCalc)
+{
+  alert("YOU ARE A POTATO COUCH!!")
+}
+else 
+{
+  alert("ERROR!");
+}
+// challenge 3 
+
+if((age >19 && bmiCalc < 18.5){
+  alert("you are UNDERWEIGHT!!! you flat stick");
+}
+else if(18.5<bmiCalc && bmiCalc<24.9)
+{
+  alert("YOU ARE HEALTHY!!!! daaaaaaamn")
+}
+else if(25.0<bmiCalc && bmiCalc<29.9){
+  alert("YOU ARE OVERWEIGHT!!!! start a diet!")
+}
+else if (30<bmiCalc)
+{
+  alert("YOU ARE A POTATO COUCH!!")
+}
+
 }
